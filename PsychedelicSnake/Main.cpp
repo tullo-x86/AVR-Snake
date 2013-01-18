01a0aef3bb9fee4a1f33b3a2320682534c8db176
Tullo.x86 <d.tullemans@gmail.com>
2013-01-18 23:07:40 +1000

    First pseudocode for rendering

--- PsychedelicSnake/Main.cpp
+++ PsychedelicSnake/Main.cpp
@@ -1,6 +1,13 @@
+#include "Snake.h"
 
+void render(SnakeGame*);
 
 void main()
 {
-	//int f = asd;
+	SnakeGame game = SnakeGame(render);
+}
+
+void render(SnakeGame* game) {
+	// Go screw with PixelToaster's buffer and tell it to render
+	// or whatever it is the kids do these days
 }
